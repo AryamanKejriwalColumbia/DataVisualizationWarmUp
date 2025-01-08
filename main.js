5949defa81bc0c67813a6e0a513c73078d9e66ba
AryamanKejriwalColumbia <123268105+AryamanKejriwalColumbia@users.noreply.github.com>
2025-01-08 15:49:56 +0530

    Update main.js to contain most of the code

--- main.js
+++ main.js
@@ -1,10 +1,76 @@
-console.log('Hello world')
+// Color Palette:
+// Background -> F2E9E4
+// Seconds -> 9A8C98
+// Minutes -> 4A4E69
+// Hours -> 22223B
+// Ticks -> C9ADA7
+
+class Time {
+  constructor(h, m, s) {
+    this.hour = h;
+    this.minute = m;
+    this.second = s;
+  }
+  
+  get_time() {
+    return [this.hour, this.minute, this.second]
+  }
+}
+
+let current_time = new Time(0, 0, 0);
+let last_time = new Time(0, 0, 0);
 
 function setup() {
-  createCanvas(800, 600);
-  background(0);
+  createCanvas(400, 400);
+  current_time = new Time(hour(), minute(), second());
 }
 
 function draw() {
-  background(0);
+  background("#F2E9E4");
+  
+  last_time = current_time;
+  current_time = new Time(hour(), minute(), second());
+  
+  if(current_time.get_time()[1] != last_time.get_time()[1]) {
+    console.log(current_time.get_time()[1]);
+  }
+  
+  t = current_time.get_time();
+  
+  
+  display_ticks();
+  display_time(t[0]%12, t[1], t[2]);
+}
+
+function display_ticks() {
+  
+}
+
+function display_time(hour, minute, second) {
+  
+  translate(width/2, height/2);
+  rotate(PI + HALF_PI);
+  translate(-width/2, -height/2);
+  
+  fill("#9A8C98");
+  if(hour != 0) {
+    arc(width/2, height/2, width*0.65, height*0.65, 0, map(hour, 0, 12, 0, TWO_PI), PIE);
+  } else {
+    arc(width/2, height/2, width*0.65, height*0.65, 0, 0.07, PIE);
+  }
+  
+  fill("#4A4E69");
+  if(minute != 0) {
+    arc(width/2, height/2, width*0.5, height*0.5, 0, map(minute, 0, 60, 0, TWO_PI), PIE);
+  } else {
+    arc(width/2, height/2, width*0.5, height*0.5, 0, 0.07, PIE);
+  }
+  
+  noStroke();
+  fill("#22223B");
+  if(second != 0) {
+    arc(width/2, height/2, width*0.35, height*0.35, 0, map(second, 0, 60, 0, TWO_PI), PIE);
+  } else {
+    arc(width/2, height/2, width*0.35, height*0.35, 0, 0.07, PIE);
+  }
 }
